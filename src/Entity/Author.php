dad1c5ee0ee884214fb387856ab5d2dc097101a4
Abdel Bailoul <ab.bailoul@gmail.com>
2023-04-25 00:36:39 +0200

    added author entity

---
+++ src/Entity/Author.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuthorRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AuthorRepository::class)]
+class Author
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $fistname = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $lastname = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFistname(): ?string
+    {
+        return $this->fistname;
+    }
+
+    public function setFistname(string $fistname): self
+    {
+        $this->fistname = $fistname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+}
